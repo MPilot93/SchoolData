e101826846c44a291a07f1d05a4fab450f7a1c86
marco <marco.plt.93@gmail.com>
2022-05-04 10:47:36 +0200

    Handler e persister student

---
+++ Persister/HandlerStudent.cs
@@ -0,0 +1,28 @@
+﻿
+using SchoolLibrary;
+
+
+namespace Persister
+{
+    public class HandlerStudent
+    {
+        private readonly string connectionString = "Server=.;Database=Gestionale;Trusted_Connection=True;";
+
+        public bool InsertStudent()
+        {
+            var student = new Student()
+            {
+                IdStudent = 1,
+                Matricola = "1111",
+                Registration = new DateTime(2015, 1, 1),
+            };
+
+            var persister = new StudentPersister(connectionString);
+            return persister.Add(student);
+        }
+
+     
+        }
+    }
+
+}
--- Persister/StudentPersister.cs
+++ Persister/StudentPersister.cs
@@ -1,30 +1,100 @@
-﻿using System.Data.SqlClient;
-using SchoolLibrary;
+﻿using SchoolLibrary;
+using System.Data.SqlClient;
 
 namespace Persister
 {
     public class StudentPersister
     {
-        public bool AddStudent(Student student)
+        private readonly string ConnectionString;
+        public StudentPersister(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public bool Add(Student student)
         {
             var sql = @"
                         INSERT INTO [dbo].[Student]
-                                   ([IdPerson]
+                                   ([IdStudent]
                                    ,[Matricola]
-                                   ,[DataIscrizione])
+                                   ,[Registration]
+                                   
                              VALUES
-                                   (@IdPerson
+                                   (@IdStudent
                                    ,@Matricola
-                                   ,@DataIscrizione)";
+                                   ,@Registration)";
 
-            using var connection = new SqlConnection(CONNECTION_STRING);
+            using var connection = new SqlConnection(ConnectionString);
             connection.Open();
             using var command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@IdPerson", student.Id);
+            command.Parameters.AddWithValue("@IdStudent", student.IdStudent);
             command.Parameters.AddWithValue("@Matricola", student.Matricola);
-            command.Parameters.AddWithValue("@DataAssunzione", student.DataIscrizione);
-
+            command.Parameters.AddWithValue("@Registration", student.Registration);
+            
             return command.ExecuteNonQuery() > 0;
         }
+
+            public IEnumerable<Student> GetStudent(int IdStudent)
+        {
+
+            var sql = @"
+                           [SELECT[IdStudent]
+                          ,[Matricola]
+                          ,[Registration]
+
+
+                      FROM[dbo].[Student]";
+
+
+            using var connection = new SqlConnection(ConnectionString);
+            connection.Open();
+            using var command = new SqlCommand(sql, connection);
+           
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                yield return new Student
+                {
+                    Registration = Convert.ToDateTime(reader["Registration"]),
+
+                    IdStudent = Convert.ToInt32(reader["IdStudent"]),
+                    Matricola = reader["Matricola"].ToString(),
+                };
+
+            }
+
+        }
     }
+
 }
+//public bool Delete(int Idstudent)
+//{
+//    var sql = @"DELETE FROM [dbo].[Person]
+//                WHERE Id=@Id ";
+//    using var connection = new SqlConnection(ConnectionString);
+//    connection.Open();
+//    using var command = new SqlCommand(sql, connection);
+//    command.Parameters.AddWithValue("@Id", 1);
+//    return command.ExecuteNonQuery() > 0;
+//}
+
+
+//public bool Update(Person person)
+//{
+//    var sql = @"UPDATE [dbo].[Person]
+//               SET [Name] = @Name
+//                  ,[Surname] = @Surname
+//                  ,[BirthDay] = @BirthDay
+//                  ,[Gender] = @Gender
+//             WHERE @Id=Id";
+
+//    using var connection = new SqlConnection(ConnectionString);
+//    connection.Open();
+//    using var command = new SqlCommand(sql, connection);
+//    command.Parameters.AddWithValue("@Name", person.Name);
+//    command.Parameters.AddWithValue("@Surname", person.Surname);
+//    command.Parameters.AddWithValue("@BirthDay", person.Birthday);
+//    command.Parameters.AddWithValue("@Gender", person.Gender);
+//    command.Parameters.AddWithValue("@Id", person.Id);
+//    return command.ExecuteNonQuery() > 0;
+//}
